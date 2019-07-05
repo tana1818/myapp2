f15aa122dcf4ba221248954b634cd8b9228b84ef
tana1818 <tanakagen1124@gmail.com>
2019-07-06 01:55:06 +0900

    first_commit

---
+++ db/migrate/20190705165355_create_todos.rb
@@ -0,0 +1,11 @@
+class CreateTodos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :todos do |t|
+      t.string :name
+      t.text :text
+      t.integer :type
+
+      t.timestamps
+    end
+  end
+end
